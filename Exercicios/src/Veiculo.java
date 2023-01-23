dbf00b01d5cf03577c182383afb0290ab1af2690
EdneyPaes <edney19982010@gmail.com>
2023-01-23 11:12:45 -0300

    Exercicios Herança resolvido

---
+++ Exercicios/src/Veiculo.java
@@ -0,0 +1,11 @@
+/* exercicios 1: Crie a classe "Veiculo","Carro", "Moto" e "Caminhao". ​
+​
+Obs: use a linguagem que gostar e siga as dicas sobre como criar classes. Faça a relação de herança que julgue adequada. */
+
+
+class Veiculo {
+
+    //Atributos e métodos
+
+}
+//As outras 3 classes, carro, moto e caminhão, são subtipos de Veiculo, pq? pq na classe veiculo vai ter todos os comportamentos de um veiculo, isto é, correr, parar, dar seta etc... moto, caminhao e carro tem esses comportamentos, sendo assim, moto, caminhao e carro vai HERDAR os comportamentos da classe veiculo, isto é, reutilizar atributos e métodos de veiculos.
